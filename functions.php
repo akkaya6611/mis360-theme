193fca18ad326348185099b4d74dd1bd6de66cc5
akkaya6611 <132543380+akkaya6611@users.noreply.github.com>
2026-08-29 01:54:10 +0300

    Performance: Add explicit width and height attributes to logo images to prevent Cumulative Layout Shift (CLS), v1.1.1

--- functions.php
+++ functions.php
@@ -17,7 +17,7 @@ if (!defined('ABSPATH')) {
 /**
  * Tema Sabitleri (Constants)
  */
-define('MIS360_VERSION', '1.1.0');
+define('MIS360_VERSION', '1.1.1');
 define('MIS360_DIR', get_template_directory());
 define('MIS360_URI', get_template_directory_uri());
 

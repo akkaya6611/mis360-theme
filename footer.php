1971cfd9006c39b7b158d7f0a60374d30866fe17
akkaya6611 <132543380+akkaya6611@users.noreply.github.com>
2026-08-28 17:32:27 +0300

    Make theme completely standalone (Download external images & rewrite URLs)

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
 $phone         = '0535 830 93 07';
 $clean_phone   = '+905358309307';
 $whatsapp      = '905358309307';
-$logo_url      = '<?php echo get_template_directory_uri(); ?>/assets/img/demo/cropped-Basliksiz-1-1.png';
+$logo_url      = get_template_directory_uri() . '/assets/img/demo/cropped-Basliksiz-1-1.png';
 $anchor_prefix = is_front_page() ? '' : home_url('/');
 ?>
 
